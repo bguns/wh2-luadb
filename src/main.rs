d87d058fc505dd6d348f07c784b3f8a9a49487dd
Bert Guns <bertguns@gmail.com>
2021-01-13 10:29:42 +0100

    Add config struct and parse args into config

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,81 @@
-use clap::{load_yaml, App};
-use std::path::Path;
+use clap::{load_yaml, App, ArgMatches};
+use std::path::PathBuf;
 use std::process::Command;
+
+struct Config {
+    rpfm_path: PathBuf,
+    packfile: Option<PathBuf>,
+    _in_dir: Option<PathBuf>,
+    out_dir: PathBuf,
+}
+
+impl Config {
+    fn from_matches(matches: &ArgMatches) -> Config {
+        let rpfm_path: PathBuf = if let Some(rpfm) = matches.value_of("rpfm-path") {
+            PathBuf::from(rpfm)
+        } else {
+            println!("[ERROR] Please provide a path to rpfm_cli.exe");
+            panic!();
+        };
+
+        if !rpfm_path.exists() {
+            println!("[ERROR] path to RPFM cli not found");
+            panic!();
+        }
+
+        let packfile_path = if let Some(packfile) = matches.value_of("packfile") {
+            Some(PathBuf::from(packfile))
+        } else {
+            None
+        };
+
+        let in_dir_path = if let Some(directory) = matches.value_of("directory") {
+            Some(PathBuf::from(directory))
+        } else {
+            None
+        };
+
+        let out_dir_path = if let Some(output_dir) = matches.value_of("output-dir") {
+            PathBuf::from(output_dir)
+        } else {
+            if let Some(ref packfile) = packfile_path {
+                let packfile_dir = packfile.parent().unwrap();
+                let packfile_name = packfile.file_stem().unwrap();
+                let mut dir = PathBuf::from(packfile_dir);
+                dir.push(&format!("{0}_lua_ext", packfile_name.to_str().unwrap()));
+                dir
+            } else {
+                PathBuf::new()
+            }
+        };
+
+        Config {
+            rpfm_path,
+            packfile: packfile_path,
+            _in_dir: in_dir_path,
+            out_dir: out_dir_path,
+        }
+    }
+}
 
 fn main() {
     let yaml = load_yaml!("cli.yaml");
     let matches = App::from(yaml).get_matches();
 
-    let mut rpfm_path_string = String::new();
+    let config = Config::from_matches(&matches);
 
-    if let Some(rpfm) = matches.value_of("rpfm-path") {
-        println!("Value for rpfm-path: {}", rpfm);
-        rpfm_path_string = rpfm.to_owned();
-    }
+    println!("Running RPFM Command");
 
-    let rpfm_path = Path::new(&rpfm_path_string);
-
-    if !rpfm_path.exists() {
-        println!("[ERROR] path to RPFM cli not found");
-        return;
-    }
-
-    if let Some(packfile) = matches.value_of("packfile") {
-        let out_dir = if let Some(output_dir) = matches.value_of("output-dir") {
-            output_dir.to_owned()
-        } else {
-            let packfile_dir = Path::new(packfile).parent().unwrap();
-            let packfile_name = Path::new(packfile).file_stem().unwrap();
-            packfile_dir
-                .join(Path::new(&format!(
-                    "{0}_lua_ext",
-                    packfile_name.to_str().unwrap()
-                )))
-                .to_str()
-                .unwrap()
-                .to_owned()
-        };
-
-        println!("Packfile: {0}, out_dir: {1}", packfile, out_dir);
-        println!("Running RPFM Command");
-
-        Command::new(&rpfm_path)
+    if let Some(ref packfile) = config.packfile {
+        Command::new(&config.rpfm_path)
             .args(&[
                 "-g",
                 "warhammer_2",
                 "-p",
-                &format!("{0}", packfile),
+                &format!("{0}", &packfile.to_str().unwrap()),
                 "packfile",
                 "-E",
-                &format!("{0}", out_dir),
+                &format!("{0}", &config.out_dir.to_str().unwrap()),
                 "-",
                 "db",
             ])
@@ -55,9 +83,5 @@ fn main() {
             .expect("unable to run rpfm")
             .wait()
             .expect("error waiting for rpfm");
-    }
-
-    if let Some(directory) = matches.value_of("directory") {
-        println!("Value for directory: {}", directory);
     }
 }
